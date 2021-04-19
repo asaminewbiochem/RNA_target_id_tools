8326632f78cbd72d8cca7cf9e178f34ff9aeb4be
aahaile <ashaethio@gmail.com>
2021-04-19 09:01:49 -0400

    keep training

--- code/concate.py
+++ code/concate.py
@@ -1,18 +1,20 @@
 import argparse
 import sys
 import os
+from pathlib import Path
 
 
 parser = argparse.ArgumentParser(description='Concating files')
 parser.add_argument("--infile", default='a.inp', help='input files name')
 parser.add_argument("--outfile", default='a.out', help='output filea')
 args = parser.parse_args()
-infiles = args.infile
+
 outfiles = open("hbond-data/"+args.outfile, 'w')
 for i in range(0, 900, 50):
-    with open("tmp/"+infiles+"."+str(i)+".dat", 'r') as f:
-        for line in f:
-            line = line.strip("\n")
-            outfiles.write(line)
-            outfiles.write('\n')
+    if Path("tmp/"+args.infil+"."+str(i) + ".dat").is_file():
+        with open("tmp/"+args.infil+"."+str(i)+".dat", 'r') as f:
+            for line in f:
+                line = line.strip("\n")
+                outfiles.write(line)
+                outfiles.write('\n')
 outfiles.close()
